5ff79f0de62f6e6133d132982fbc7719b471481b
Baird-weng <644672334@qq.com>
2018-08-16 20:10:25 +0800

    多页面的iOS应用。

---
+++ src/Login.vue
@@ -0,0 +1,17 @@
+<template>
+    <text>我是登录页面呢</text>
+</template>
+
+<script>
+    export default {
+        name: '',
+        data () {
+            return {}
+        },
+        methods: {}
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,3 @@
+<template>
+  <text class="message">为什么要用text标签呢，卧槽你大爷</text>
+</template>
